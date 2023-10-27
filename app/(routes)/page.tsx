1242fb53169611c694451f2a693a7e63a757306d
Royweru <royezi17@gmail.com>
2023-10-27 11:40:50 +0300

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   app/(routes)/page.tsx 	new file:   app/favicon.ico 	new file:   app/globals.css 	new file:   app/layout.tsx 	new file:   components/Footer.tsx 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+       <div className=' text-lg font-bold '>
+         Hellow store this is it
+       </div>
+    </main>
+  )
+}
