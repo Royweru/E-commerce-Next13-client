8ffe7f84ad36c2d81d7f5a4233694012eee1bb73
Royweru <royezi17@gmail.com>
2023-10-30 07:52:04 +0300

    modified:   app/(routes)/page.tsx 	modified:   components/billboard.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const billboard = await getBillboard("1132e424-1630-4a73-81c0-71181d1aa176")
   console.log(billboard)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
+    <main className="flex min-h-screen flex-col justify-between w-full ">
       <Container>
         <div className=' space-y-10 '>
           <Billboard data={billboard} />
